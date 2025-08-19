0c86194efd39722bd61bb39fb82552c89f57b4e1
Your Name <bahrom9791@gmail.com git config --global user.name>
2025-08-19 20:58:52 +0500

    add python-dotenv

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.types import FSInputFile
 from aiogram.filters import Command
 from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 from dotenv import load_dotenv
 
 # Локальные конвертеры
@@ -20,7 +21,10 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 logging.basicConfig(level=logging.INFO)
-bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+bot = Bot(
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
 dp = Dispatcher()
 
 @dp.message(Command("start"))
