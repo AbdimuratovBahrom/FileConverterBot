9cd0d219fbd2c17baa13ed488e8f4fc444f9bce1
Your Name <bahrom9791@gmail.com git config --global user.name>
2025-08-25 17:48:58 +0500

    add python-dotenv

--- converters/audio.py
+++ converters/audio.py
@@ -1,11 +1,22 @@
+
+
 import os
 import tempfile
-from pydub import AudioSegment
+import ffmpeg
 
 
 def convert_audio_file(input_file: str, target_format: str) -> str:
-    output_file = os.path.join(tempfile.gettempdir(),
-                               os.path.splitext(os.path.basename(input_file))[0] + f".{target_format}")
-    audio = AudioSegment.from_file(input_file)
-    audio.export(output_file, format=target_format)
+    output_file = os.path.join(
+        tempfile.gettempdir(),
+        os.path.splitext(os.path.basename(input_file))[0] + f".{target_format}"
+    )
+
+    (
+        ffmpeg
+        .input(input_file)
+        .output(output_file)
+        .run(overwrite_output=True)
+    )
+
     return output_file
+
